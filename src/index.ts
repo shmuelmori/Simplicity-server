defd000b306869c3209cb608e18174d37ceff583
shmuelmori <shmuelmuri@gmail.com>
2024-10-30 02:03:27 +0200

    adding new site origin

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5001;
 
 
 app.use(cors({
-  origin: ['http://localhost:5173'], // Your frontend origin
+  origin: ["https://simplicity-project.netlify.app/", 'http://localhost:5173'], // Your frontend origin
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
